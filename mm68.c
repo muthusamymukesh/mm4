8e6a6f3363547f5f593a0b5e19068718cf3c9b9e
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-03-01 16:19:51 +0530

    Create mm68.c

---
+++ mm68.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,c;
+    printf("enter the value");
+    scanf("%d",&a);
+    
+    if(a%7==0)
+    {printf("\nyes");
+    }
+    else
+    {
+    printf("\nnot");    
+    }
+}
