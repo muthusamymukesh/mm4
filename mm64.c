2303659b2cc40f89c2ffa9bfe5e451f60422425e
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-03-01 16:12:20 +0530

    Create mm64.c

---
+++ mm64.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the value");
+    scanf("%d\t%d",&a,&b);
+    c=a+b;
+    if(c%2==0)
+    {printf("%d is even",c);
+    }
+    else
+    {
+    printf("%d is odd",c);    
+    }
+}
