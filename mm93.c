3dd2f2857ce8e1658801bf27ff63282bc15e6d82
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-03-13 16:41:10 +0530

    Create mm93.c

---
+++ mm93.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    
+    int a,b;
+    printf("Enter the temperature ");
+    scanf("%d",&a);
+    b=a+273;
+
+    printf("\nTemperature of %d is in kelvin is %d.",a,b);
+}
