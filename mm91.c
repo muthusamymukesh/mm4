14cf393e84159a55acc25c3462440f6f905253bb
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-03-13 16:24:42 +0530

    Create mm91.c

---
+++ mm91.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    
+    int b,l,h,v;
+    printf("Enter the length,width,hight");
+    scanf("%d\t%d\t%d",&l,&b,&h);
+    v=l*b*h;
+    printf("\n the volume of cube is %d",v);
+}
