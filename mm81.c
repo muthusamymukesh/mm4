bd7d7a535465d2838f10c8be0c1086b8ea6ce7d9
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-03-16 14:25:20 +0530

    Create mm81.c

---
+++ mm81.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d,e,f,i,j,k,l;
+    printf("enter the value");
+    scanf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d",&a,&b,&c,&d,&e,&f);
+    i=b-a;
+    j=d-c;
+    k=f-e;
+    printf("%d\n%d\n%d",i,j,k);
+}
