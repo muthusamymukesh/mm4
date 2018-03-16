bd805ea9aa075abfc5ae1e45cc19c6c54f5b01c3
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-03-16 14:19:30 +0530

    Create mm83.c

---
+++ mm83.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d,e,f,g,h,i,j,k,l;
+    printf("enter the value");
+    scanf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d",&a,&b,&c,&d,&e,&f,&g,&h);
+    i=a/b;
+    j=c%d;
+    k=e/f;
+    l=g%h;
+    printf("%d\n%d\n%d\n%d.",i,j,k,l);
+}
