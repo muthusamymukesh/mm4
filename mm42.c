f4e19c5922cf9147374b2aed97c711577ad7c22e
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-03-20 16:40:10 +0530

    Create mm42.c

---
+++ mm42.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+	char a[100],b[100];
+	int c,d;
+	printf("enter");
+	scanf("&s\t&s",a,b);
+	c=strlen(a);
+	d=strlen(b);
+	if(a>b)
+	{
+	    printf("%s",a);
+	}
+	else
+	{
+	    printf("%s",b);
+	}}
+	
